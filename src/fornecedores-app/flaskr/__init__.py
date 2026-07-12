28cfef358d6b9930de3d58cdbe4000178f8c143e
izabelacunha <161177518+izabelacunha@users.noreply.github.com>
2026-07-12 13:24:36 +0000

    feat(db): setup sqlite database and create providers schema (close #2)

--- src/fornecedores-app/flaskr/__init__.py
+++ src/fornecedores-app/flaskr/__init__.py
@@ -26,4 +26,7 @@ def create_app(test_config=None):
     def hello():
         return 'Olá! Bem-vindo ao Gerenciador de Fornecedores de Eventos!'
 
+    from . import db
+    db.init_app(app)
+
     return app
