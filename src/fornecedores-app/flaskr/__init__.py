bb9f1e2b17e66865a3d4aa3190ffbc981ddef728
izabelacunha <161177518+izabelacunha@users.noreply.github.com>
2026-07-12 13:17:51 +0000

    feat(base): setup application factory and initial configuration for providers app (refs #2)

---
+++ src/fornecedores-app/flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    # Cria e configura o aplicativo Flask
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'fornecedores.sqlite'),
+    )
+
+    if test_config is None:
+        # Carrega a configuração da instância, se existir, quando não estiver testando
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Carrega a configuração de teste se passada
+        app.config.from_mapping(test_config)
+
+    # Garante que a pasta da instância exista
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Olá! Bem-vindo ao Gerenciador de Fornecedores de Eventos!'
+
+    return app
